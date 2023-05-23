775668f3ffa3e953990614fcbc709eefed5a924b
mohandturky2002 <mohandhany35@gmail.com>
2023-05-23 14:14:09 +0300

    test

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const dbConnect = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://MohandHany:ttuurrkkyy2002@cluster0.pk6jbgm.mongodb.net/Next-E-commerce"
+      "mongodb+srv://MohandHany:ttuurrkkyy2002@cluster0.pk6jbgm.mongodb.net/"
     );
     console.log("connected successfully");
   } catch (error) {
