b17b774f3135a659b8f8590a8a2f9571f4998cfd
mohandturky2002 <mohandhany35@gmail.com>
2023-05-23 12:14:09 +0300

    blog-api

---
+++ pages/api/api/posts/[postId].js
@@ -0,0 +1,36 @@
+import Posts from "@/models/Post";
+import dbConnect from "@/utils/dbConnect";
+
+const handlerDeletPost = async (req, res) => {
+  if (req.method === "DELETE") {
+    try {
+      await dbConnect();
+      const post = await Posts.findByIdAndDelete({ _id: req.query.postId });
+      res.json({ post });
+    } catch (error) {
+      res.json({ error });
+    }
+  } else if (req.method === "GET") {
+    try {
+      await dbConnect();
+      const post = await Posts.findOne({ _id: req.query.postId });
+      res.json({ post });
+    } catch (error) {
+      res.json({ error });
+    }
+  } else if (req.method === "PUT") {
+    try {
+      await dbConnect();
+      const post = await Posts.findOne({ _id: req.query.postId });
+      (post.titlePost = req.body.titlePost),
+        (post.imgPost = req.body.imgPost),
+        (post.discPost = req.body.discPost),
+        await post.save();
+      res.json({ post });
+    } catch (error) {
+      res.json({ error });
+    }
+  }
+};
+
+export default handlerDeletPost;
---
+++ pages/api/api/posts/index.js
@@ -0,0 +1,24 @@
+import Posts from "@/models/Post";
+import dbConnect from "@/utils/dbConnect";
+
+const handlerGetPosts = async (req, res) => {
+  if (req.method === "GET") {
+    try {
+      await dbConnect();
+      const post = await Posts.find({}, { __v: 0 });
+      res.json({ post });
+    } catch (error) {
+      res.json({ error });
+    }
+  } else if (req.method === "POST") {
+    try {
+      await dbConnect();
+      const post = await Posts.create(req.body);
+      res.json({ post });
+    } catch (error) {
+      res.json({ error });
+    }
+  }
+};
+
+export default handlerGetPosts;
